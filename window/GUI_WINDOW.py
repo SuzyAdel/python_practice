63db62990d3f46f70d06cfdebbf9951b72156d42
SuzyAdel <suzyadel37@gmail.com>
2023-09-17 13:48:41 +0300

    added a button to the basic window & lable

--- window/GUI_WINDOW.py
+++ window/GUI_WINDOW.py
@@ -1,16 +1,16 @@
 from tkinter import *
 
-window= Tk()
+window= Tk()#init window
 window.geometry("420x420")
 window.title("Suzy's 1st GUI program")
 
 
-icon= PhotoImage(file='logo.png')
+icon= PhotoImage(file='logo.png')#creating logo as icon photo
 window.iconphoto(True,icon)
 window.config(background="#5cfcff")
 
-photo= PhotoImage(file='person.png')
-label=Label(window,text="Hello World, LETS START CODING!",
+photo= PhotoImage(file='person.png') #adding a label and main photo
+label=Label(window,text="LETS START CODING!",
             font=('Arial',40,'bold'),
             fg='blue',bg="white",
             relief=RAISED,bd=10,
@@ -19,6 +19,23 @@ label=Label(window,text="Hello World, LETS START CODING!",
             compound='top')  #fg=foreground of lable, bd border width
 
 label.pack() #puts anywhere
-#label.place(x=100,y=100), cordinates 
+#label.place(x=100,y=100), cordinates
+
+#creating button + click & photo
+count=0
+def click():
+    global count
+    count+=1
+    print(count)
+photo2=PhotoImage(file="like.png")
+button=Button(window,text="Like button",command=click,
+              font=("comic Sans",30),
+              fg="#f2d40c",bg="black",
+              activeforeground="#f2d40c",
+              activebackground="#f20c87",
+              state=ACTIVE,
+              image=photo2, compound=BOTTOM)
+button.pack()
+
 
 window.mainloop()
